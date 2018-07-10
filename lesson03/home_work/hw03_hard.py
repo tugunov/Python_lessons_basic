ae4d32800ec0d9d3b141f4a3bae56c1f9bd2821d
tugunov <tugunov@gmail.com>
2018-07-10 12:45:42 +0300

    Добавлено решение hard-2

--- lesson03/home_work/hw03_hard.py
+++ lesson03/home_work/hw03_hard.py
@@ -18,6 +18,76 @@
 # они получают удвоенную ЗП, пропорциональную норме.
 # Кол-во часов, которые были отработаны, указаны в файле "data/hours_of"
 
+import os
+
+p_workers = os.path.join('data','workers')
+p_hours = os.path.join('data','hours_of')
+
+f_w = open(p_workers, 'r', encoding='UTF-8')
+f_h = open(p_hours, 'r', encoding='UTF-8')
+
+w_string = f_w.readlines()
+h_string = f_h.readlines()
+
+f_w.close()
+f_h.close()
+
+w_table = [] # для хранения и работы с таблицами данных
+h_table = []
+res_table = []
+
+for line in w_string: # преобразовываем файлы в двумерные списки-таблицы
+	s = line.replace('\n', '')
+	s = s.split(' ')
+	s = list(filter(lambda x: x!='', s))
+	w_table.append(s)
+
+for line in h_string:
+	s = line.replace('\n', '')
+	s = s.split(' ')
+	s = list(filter(lambda x: x!='', s))
+	h_table.append(s)
+
+w_table.pop(0) #удаляем заголовки в таблицах
+h_table.pop(0)
+
+print(w_table)
+print(h_table)
+
+for line in w_table:
+	name = line[0]
+	surname = line[1]
+	salary = float(line[2])
+	norm_hours = int(line[4])
+	for h_line in h_table:
+		if (h_line[0] == name) & (h_line[1] == surname):
+			fact_hours = int(h_line[2])
+
+	if fact_hours <= norm_hours:
+		salary = salary * (fact_hours/norm_hours)
+	else:
+		salary = salary * (1 + (fact_hours - norm_hours) / norm_hours)
+
+	res_table.append([name, surname, str(round(salary, 2))])
+
+	# print('{} {} {}'.format(name, surname, round(salary, 2)))
+
+# красивый вывод на печать данных о фактической зарплате
+name_max = 0
+surname_max = 0
+salary_max = 0
+
+for line in res_table:
+	if len(line[0])>name_max:
+		name_max = len(line[0])
+	if len(line[1])>surname_max:
+		surname_max = len(line[1])
+	if len(line[2])>salary_max:
+		salary_max = len(line[2])
+
+for line in res_table:
+	print('{}{} {}{} {}'.format(line[0], ' '*(name_max - len(line[0])),
+		line[1], ' '*(surname_max - len(line[1])), line[2]))
 
 # Задание-3:
 # Дан файл ("data/fruits") со списком фруктов.
