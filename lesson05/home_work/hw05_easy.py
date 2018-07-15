62112f7c417526ebdefb60b75a57fadad08ae3a0
tugunov <tugunov@gmail.com>
2018-07-15 20:13:36 +0300

    Добавлено решение 05easy-1

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -3,6 +3,32 @@
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+def make_dir():
+    import os
+    for n in '0123456789':
+        dir_path = os.path.join(os.getcwd(), 'dir_' + n)
+        try:
+            os.mkdir(dir_path)
+            print('Директория dir_{} создана.'.format(n))
+        except FileExistsError:
+            print('Директория {} уже существует.'.format(dir_path))
+
+def del_dir():
+    import os
+    for n in '0123456789':
+        dir_path = os.path.join(os.getcwd(), 'dir_' + n)
+        try:
+            os.rmdir(dir_path)
+            print('Директория dir_{} удалена.'.format(n))
+        except PermissionError:
+            print('Директория на удаление не пустая.')
+
+
+make_dir()
+
+# del_dir()
+
+
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
